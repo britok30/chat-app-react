053398653fac9d26599bccaca0cc12129c0d5f26
Kelvin Brito <britok30@gmail.com>
2021-03-17 23:17:16 -0400

    chat styles

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="e85356e7-bb67-495e-864f-e538efe4a5bd"
+            userName="kbrito30"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+};
+
+export default App;
